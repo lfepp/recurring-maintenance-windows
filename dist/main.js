ecfdc745447bb1049b347d52e607e73e8c3cadf8
Luke Epp <lucasfepp@gmail.com>
2016-06-28 12:08:10 -0700

    Changing the way core is called

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,5 @@
 'use strict';
 
-console.log('running');
+console.log('Initializing maintenance window creation...');
 
- $.getScript('core.js')
-    .done(function(script, textStatus) {
-      console.log(textStatus);
-      script.initialize();
-    })
-    .fail(function(jqxhr, settings, exception) {
-      console.error('Trigger ajax error: ' + exception);
-    });
+initialize();
