8e9e5743d3eb574d29aa38380ae4757b6f58de3c
Luke Epp <lucasfepp@gmail.com>
2016-06-28 12:03:14 -0700

    Making server.js es5

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict';
 
-import express from 'express';
-const app = express();
+var express = require('express');
+var app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-const port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('Server listening on port ' + port);
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 });
 
-export default app;
+module.exports = app;
