7d0e2f5ca14c0562112adef3ac60a90c93c2cc2b
Luke Epp <lucasfepp@gmail.com>
2016-06-28 11:27:54 -0700

    Added bundle workflow for front-end and start script

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('running');
+
+ $.getScript('core.js')
+    .done(function(script, textStatus) {
+      console.log(textStatus);
+      script.initialize();
+    })
+    .fail(function(jqxhr, settings, exception) {
+      console.error('Trigger ajax error: ' + exception);
+    });
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 5000);
-console.log('Server listening on port ' + process.env.PORT || '5000');
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log('Server listening on port ' + port);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html');
