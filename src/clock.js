e626a20c9b979a0244217860ccfa84f67762084a
Luke Epp <lucasfepp@gmail.com>
2016-07-06 11:34:47 -0700

    Completed custom clock process

--- src/clock.js
+++ src/clock.js
@@ -2,7 +2,7 @@ var CronJob = require('cron').CronJob;
 var worker = require('./compiled_core.js');
 
 new CronJob({
-  cronTime: "00 00 12 * * *", // everyday at 12:00:00
+  cronTime: "00 00 6,12,18 * * *", // everyday at 6:00:00, 12:00:00, and 18:00:00
   onTick: worker.default(),
   start: true,
   timeZone: "America/Los_Angeles"
